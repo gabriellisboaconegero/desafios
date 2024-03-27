4ec24c1b2bf1b8a168a383c3f60d0a8dbb5c2c29
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-03-26 22:42:41 -0300

    semana 3: f

--- semana3/f.cpp
+++ semana3/f.cpp
@@ -6,26 +6,31 @@ int main(){
     ll n;
     cin >> n;
     vector<ll> v(n);
+    vector<ll> rs(n-1);
     for (auto &i : v) cin >> i;
 
     sort(v.begin(), v.end());
 
-    int change_idx = -1;
-    for (int i = 1; i < n-1; i+=2){
-        if (v[i]/v[i-1] != v[i+1]/v[i])
-            change_idx = i;
+    ll rm = numeric_limits<ll>::max();
+    ll rM = numeric_limits<ll>::min();
+    for (int i = 1; i < n; i++){
+        rm = min(rm, v[i]/v[i-1]);
+        rM = max(rM, v[i]/v[i-1]);
     }
+    /* 6, 5 */
+    /* 0 1 2 3 4 5 */
+    /* x x x x x x */
 
-    ll r;
-    if (change_idx != -1){
-        r = min(v[change_idx]/v[change_idx-1], v[change_idx+1]/v[change_idx]);
-        int id_wrong = v[change_idx]/v[change_idx-1] > v[change_idx+1]/v[change_idx] ? change_idx-1 : change_idx;
-        cout << v[id_wrong]*r << endl;
+    if (rm != rM){
+        int i;
+        for (i = 1; i < n; i++)
+            if (v[i]/v[i-1] == rM)
+                break;
+        cout << v[i-1]*rm << endl;
     } else {
-        r = v[1]/v[0];
-        if (v[0]%r == 0 && r != 1)
-            cout << v[0]/r << endl;
-        cout << v.back()*r << endl;
+        if (v[0]%rm == 0 && rm != 1)
+            cout << v[0]/rm << endl;
+        cout << v.back()*rm << endl;
     }
 
     return 0;
